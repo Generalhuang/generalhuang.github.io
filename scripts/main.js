df0aa57bf2b3746dc4a01471ef25b1bd975c7840
Generalhuang <30338147+Generalhuang@users.noreply.github.com>
2019-02-23 19:17:11 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,31 @@
+let myImage = document.querySelector('img');
 
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'image/mm.jpeg') {
+      myImage.setAttribute('src', 'image/firefox2.png');
+    } else {
+      myImage.setAttribute('src', 'image/mm.jpeg');
+    }
+}
+
+function setHeading(name) {
+  let myHeading = document.querySelector('h1');
+  myHeading.textContent = 'Mozilla 酷毙了，' + name + '！';
+}
+
+function setUserName() {
+  let myName = prompt('请输入你的名字');
+  localStorage.setItem('name', myName);
+  setHeading(myName);
+} 
+
+let storedName = localStorage.getItem('name');
+if(!storedName) {
+   setUserName();
+} else {
+   setHeading(storedName);
+}
+
+let myButton = document.querySelector('button'); 
+myButton.onclick = setUserName;
